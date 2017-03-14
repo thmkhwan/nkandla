520965f4b2cba07d7d166a6cd758f3bd55a823db
Nick Combrink <nickc@bbd.co.za>
2017-03-14 15:33:31 +0200

    Added Skeleton

---
+++ nkandla/src/main/java/com/bbd/App.java
@@ -0,0 +1,13 @@
+package com.bbd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
