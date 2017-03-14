dc351a72ba957c652504c6e4f2e92f49288e4bfb
Daniel <danields@bbd.co.za>
2017-03-14 16:27:28 +0200

    Initial Person tests.

--- nkandla/src/test/java/com/bbd/AppTest.java
+++ nkandla/src/test/java/com/bbd/AppTest.java
@@ -1,5 +1,7 @@
 package com.bbd;
 
+import org.junit.Before;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -41,5 +43,32 @@ public class AppTest
     public void testEnumsArc()
     {
         assertEquals(2, PersonTypeEnums.personType.Architect.rank);
+    }
+
+    Person daniel;
+
+    @Before public void initPerson()
+    {
+        daniel = new Person("Daniel", 10, "Male", PersonTypeEnums.personType.Architect);
+    }
+
+    public void testPersonName()
+    {
+        asserEquals("Daniel", daniel.getName());
+    }
+
+    public void testPersonAge()
+    {
+        asserEquals(10, daniel.getName());
+    }
+
+    public void testPersonGender()
+    {
+        asserEquals("Male", daniel.getGender());
+    }
+
+    public void testPersonType()
+    {
+        asserEquals(PersonTypeEnums.personType.Architect, daniel.getPersonType());
     }
 }
